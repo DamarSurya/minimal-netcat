14873de8551efcac781b9016d61ef63ef4d71eca
DamarSurya <hexazed1@gmail.com>
2026-03-25 16:04:37 +0700

    refactor.

--- src/main.c
+++ src/main.c
@@ -1,210 +1,247 @@
-// no ipv6
-
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+
+#include <argp.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
-#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define LogInfo(v)                                                             \
-    do {                                                                       \
-        fprintf(stdout, "[INFO]: %s\n", v);                                    \
-    } while (0);
-#define LogError(v)                                                            \
-    do {                                                                       \
-        fprintf(stderr, "%s", v);                                              \
-    } while (0);
-#define SysErr(err)                                                            \
-    do {                                                                       \
-        perror(err);                                                           \
-        exit(-1);                                                              \
-    } while (0);
-
-#define REDIR_STDIN 0             // redirected stdin
-#define REDIR_STDOUT 0            // reedirected stdout
-#define MAX_BACKLOG 1             // max backlog
-#define MAXBUF 4096 // max in buffer size/stored buffer
-                
-#define DEFAULT_ADDRESS "0.0.0.0" // default addresss
-#define SOCK_TYPE SOCK_STREAM     // default socket type
-#define DEFAULT_PORT 3000         // default port
-#define DOMAIN AF_INET            // default value for socket domain/family
-
-// socket opt
-const int enableREUSEADDR = 1;
-
-enum {
-    SENDING_MODE, // sending mode
-    LISTEN_MODE   // listen mode
-};
-
-int MODE = SENDING_MODE;
-int LISTEN_LOOPED = false;
-bool IP_IS_SET = false;
-bool PORT_IS_SET = false;
-
-struct in_addr inAddr;
-struct sockaddr_in sockAddr;
-char *addressStr;
-int port;
-
-int CreateSocket(int domain, int type, int proto) {
-    int ec;
-    if ((ec = socket(domain, type, proto)) == -1)
-        SysErr("socket");
-    return ec;
-}
-
-// send data from FILE* stream
-// return total sent bytes
-// auto exit on error via SysErr
-ssize_t SendFile(int socket, size_t size, FILE *stream, int flags);
-
-// return total bytes received
-// auto write to stdout
-// auto exit on error via SysErr
-ssize_t Recv(int socket, size_t size, int flags);
-
-int main(int argc, char **argv) {
-    const char *USAGE_MESSAGE =
-        "Usage:\t ./[program] [-l (listen mode) ] [ [ip] [port] (sending mode) ] \n\tOptions:\n\
+#define DEFAULT_ADDRESS ((in_addr_t)INADDR_ANY)
+#define DEFAULT_PORT ((uint16_t)3000)
+#define MAX_BUFFER_SIZE ((size_t)4096)
+#define USAGE_MESSAGE                                                          \
+    (const char                                                                \
+         *)"Usage:\t ./[program] [-l (listen mode) ] [ [ip] [port] (sending mode) ] \n\tOptions:\n\
+    \t\t-v: \tSet verbose mode\n\
     \t\t-l: \tSet to listen mode\n\
     \t\t-p: \tSet port\n\
     \t\t-i: \tSet ip\n\
     \t\t-r: \tLooped listen mode\n\
-    \tRedirection: ./[program] [-l > out] [ 0.0.0.0 3100 < in]\n";
+    \t\t-m: \tSet message\n\
+    \t\t-b: \tSet max_backlog or maximum connection\n\
+    \tRedirection: ./[program] [-l > out] [ [ip] [port] < in]\n"
+#define INACTIVE_SOCKET ((int)-42)
+
+bool listen_mode = false;
+bool verbose_mode = false;
+
+#define sys_err(err)                                                           \
+    do {                                                                       \
+        fprintf(stderr, "[ERROR]: ");                                          \
+        perror(err);                                                           \
+        exit(-1);                                                              \
+    } while (0);
+#define log(msg)                                                               \
+    do {                                                                       \
+        fprintf(stdout, "%s\n", msg);                                          \
+    } while (0);
+#define flog(format, ...)                                                      \
+    do {                                                                       \
+        fprintf(stdout, format, __VA_ARGS__);                                  \
+    } while (0);
+#define log_verb(msg)                                                          \
+    do {                                                                       \
+        if (verbose_mode)                                                      \
+            fprintf(stderr, "[LOG]: %s\n", msg);                               \
+    } while (0);
+#define flog_verb(format, ...)                                                 \
+    do {                                                                       \
+        if (verbose_mode)                                                      \
+            fprintf(stderr, format, __VA_ARGS__);                              \
+    } while (0);
+int send_buffer(int fd, const char *buffer, size_t n, int flags);
+int send_file(int out_fd, int in_fd, size_t count);
+int recv_buffer(int fd, char *buffer, size_t n, int flags);
+
+int main(int argc, char **argv) {
     if (argc <= 1) {
-        LogError(USAGE_MESSAGE);
-        return 1;
+        fprintf(stderr, "[ERROR]: Not enough arguments\n%s", USAGE_MESSAGE);
+        exit(-1);
     }
+    bool ip_is_set = false;
+    bool port_is_set = false;
+    bool message_is_set = false;
+    bool listen_is_looped = false;
+
+    in_addr_t local_address = DEFAULT_ADDRESS;
+    uint16_t local_port = htons(DEFAULT_PORT);
+    const char *message_ptr = "\0";
+
+    int domain = AF_INET;
+    int socket_type = SOCK_STREAM;
+    int protocol = 0;
+    int max_backlog = 10;
+    int enableREUSEADDR = 1;
+
     opterr = 0;
-    int opt;
-    while ((opt = getopt(argc, argv, "lh")) != -1) {
+    int opt, nargs = 0;
+    while ((opt = getopt(argc, argv, "lhvri:p:m:b:")) != -1) {
+        ++nargs;
         switch (opt) {
         case 'h':
-            fprintf(stderr, "%s", USAGE_MESSAGE);
+            fprintf(stderr, USAGE_MESSAGE);
             exit(1);
             break;
+        case 'm':
+            message_ptr = optarg;
+            message_is_set = true;
+            break;
+        case 'v':
+            verbose_mode = true;
+            break;
         case 'l':
-            MODE = LISTEN_MODE;
-            while ((opt = getopt(argc, argv, "ri:p:")) != -1) {
-                switch (opt) {
-                case 'r':
-                    LISTEN_LOOPED = true;
-                    break;
-                case 'i':
-                    addressStr = optarg;
-                    IP_IS_SET = true;
-                    break;
-                case 'p':
-                    port = atoi(optarg);
-                    PORT_IS_SET = true;
-                    break;
-                default:
-                    break;
-                }
-            }
+            listen_mode = true;
+            break;
+        case 'r':
+            listen_is_looped = true;
+            break;
+        case 'i':
+            ip_is_set = true;
+            local_address = inet_addr(optarg);
+            break;
+        case 'p':
+            port_is_set = true;
+            local_port = htons((uint16_t)atoi(optarg));
+            break;
+        case 'b':
+            max_backlog = atoi(optarg);
             break;
         default:
             break;
         }
     }
-    if (MODE == SENDING_MODE) {
-        if (argc < 3) {
-            LogError(USAGE_MESSAGE);
-            return 1;
-        }
-        addressStr = argv[1];
-        port = atoi(argv[2]);
 
-    } else if (MODE == LISTEN_MODE) {
-        if (!IP_IS_SET)
-            addressStr = DEFAULT_ADDRESS;
-        if (!PORT_IS_SET)
-            port = DEFAULT_PORT;
-    }
-    // initialize inAddr and assign value to sockAddr
-    if (inet_pton(DOMAIN, addressStr, &inAddr) == -1)
-        SysErr("inet_pton");
-    sockAddr.sin_addr = inAddr;
-    sockAddr.sin_family = DOMAIN;
-    sockAddr.sin_port = htons(port);
-    if (MODE == SENDING_MODE) {
-        int socket;
-        socket = CreateSocket(DOMAIN, SOCK_TYPE, 0);
+    if (listen_mode) {
+        log_verb("[LOG]: On listen mode\n");
+        int local_socket = INACTIVE_SOCKET;
+        struct sockaddr_in local_socket_address;
+        local_socket_address.sin_addr.s_addr = local_address;
+        local_socket_address.sin_port = local_port;
+        local_socket_address.sin_family = domain;
+        if ((local_socket = socket(domain, socket_type, protocol)) == -1)
+            sys_err("socket");
+        if (setsockopt(local_socket, SOL_SOCKET, SO_REUSEADDR,
+                       (void *)&enableREUSEADDR,
+                       (socklen_t)sizeof(enableREUSEADDR)) == -1)
+            sys_err("setsockopt");
+        if (bind(local_socket, (struct sockaddr *)&local_socket_address,
+                 sizeof(local_socket_address)) == -1)
+            sys_err("bind");
+        if (listen(local_socket, max_backlog) == -1)
+            sys_err("listen");
+        flog_verb("[LOG]: Running on: \n\tip: %s\n\tport: %d\n\tmessage "
+                  "length: %lu\n",
+                  inet_ntoa(local_socket_address.sin_addr), ntohs(local_port),
+                  strlen(message_ptr));
 
-        if (connect(socket, (struct sockaddr *)&sockAddr, sizeof(sockAddr)) ==
-            -1)
-            SysErr("connect");
-        if (isatty(STDIN_FILENO) == REDIR_STDIN) {
-            SendFile(socket, MAXBUF, stdin, 0);
-            shutdown(socket, SHUT_WR);
-        }
-        close(socket);
-    } else if (MODE == LISTEN_MODE) {
-        int localSocket;
-        socklen_t localSockSize;
-        localSocket = CreateSocket(DOMAIN, SOCK_TYPE, 0);
-        localSockSize = sizeof(sockAddr);
-        if (setsockopt(localSocket, SOL_SOCKET, SO_REUSEADDR, &enableREUSEADDR,
-                   sizeof(enableREUSEADDR)) == -1) SysErr("setsockopt");
-
-        if (bind(localSocket, (struct sockaddr *)&sockAddr, localSockSize) ==
-            -1) {
-            SysErr("bind");
-        }
-        if (listen(localSocket, MAX_BACKLOG) == -1) {
-            SysErr("listen");
-        }
         while (1) {
-            int peerSocket;
-            if ((peerSocket = accept(localSocket, NULL, NULL)) == -1) {
-                SysErr("accept");
+            int peer_socket = INACTIVE_SOCKET;
+            struct sockaddr peer_socket_addr;
+            struct sockaddr_in *peer_addr;
+            socklen_t peer_socklen;
+
+            log_verb("[LOG]: listening...");
+            if ((peer_socket = accept(local_socket, &peer_socket_addr,
+                                      &peer_socklen)) == -1)
+                sys_err("accept");
+            peer_addr = (struct sockaddr_in *)&peer_socket_addr;
+            char buffer[MAX_BUFFER_SIZE];
+            int len = recv_buffer(peer_socket, buffer, MAX_BUFFER_SIZE, 0);
+            log_verb("[LOG]: Accepting connection");
+            flog_verb("[LOG]: Connection info: \n\tip: %s\n\tport: "
+                      "%u\n\tmessage length received: %d\n\n",
+                      inet_ntoa(peer_addr->sin_addr),
+                      ntohs(peer_addr->sin_port), len);
+
+            shutdown(peer_socket, SHUT_RD);
+            if (message_is_set) {
+                log_verb("sending message...");
+                send_buffer(peer_socket, message_ptr, strlen(message_ptr), 0);
             }
-            Recv(peerSocket, MAXBUF, 0);
-            shutdown(peerSocket, SHUT_RD);
-            close(peerSocket);
-            if (!LISTEN_LOOPED) {
+            shutdown(peer_socket, SHUT_WR);
+            close(peer_socket);
+            if (!listen_is_looped)
                 break;
-            }
         }
-        close(localSocket);
+        close(local_socket);
+        return 0;
     }
+
+    // sending mode
+    log_verb("On sending mode");
+    if (argc < 3) {
+        fprintf(stderr, "[ERROR]: Not enough arguments\n%s", USAGE_MESSAGE);
+        return -1;
+    }
+    int local_socket = INACTIVE_SOCKET;
+    struct sockaddr_in target_socket_addr;
+    target_socket_addr.sin_addr.s_addr = inet_addr(argv[nargs + 1]);
+    target_socket_addr.sin_port = htons(atoi(argv[nargs + 2]));
+    target_socket_addr.sin_family = domain;
+    if ((local_socket = socket(domain, socket_type, protocol)) == -1)
+        sys_err("socket");
+    flog_verb("[LOG]: Connecting to: %s:%s\n",
+              inet_ntoa(target_socket_addr.sin_addr), argv[3]);
+    if (connect(local_socket, (struct sockaddr *)&target_socket_addr,
+                sizeof(target_socket_addr)) == -1)
+        sys_err("connect");
+    log_verb("Connection established.");
+
+    if (message_is_set) {
+        flog_verb("Sending message. len: %lu\n", strlen(message_ptr));
+        send_buffer(local_socket, message_ptr, strlen(message_ptr), 0);
+    }
+    if (isatty(STDIN_FILENO) == 0) {
+        struct stat buf_info;
+        fstat(STDIN_FILENO, &buf_info);
+        flog_verb("Sending file. File size: %lu\n", buf_info.st_size);
+        send_file(local_socket, 0, buf_info.st_size);
+    }
+    shutdown(local_socket, SHUT_WR);
+    close(local_socket);
     return 0;
 }
-ssize_t SendFile(int socket, size_t size, FILE *stream, int flags) {
-    char buffer[size];
-    ssize_t totalSent = 0;
-    size_t nread = 0;
-    memset(buffer, '\0', sizeof(buffer));
-    while ((nread = fread(buffer, 1, sizeof(buffer), stream)) > 0) {
-        char* ptr = buffer;
-        while (nread > 0) {
-            ssize_t len = send(socket, ptr, nread, flags);
-            if (len == -1) SysErr("send");
-            ptr += len;
-            nread -= len;
-            totalSent += len;
-        }
-    }
-    return totalSent;
-}
 
-ssize_t Recv(int socket, size_t size, int flags) {
-    char buffer[size];
-    ssize_t received = 0, totalReceived = 0;
-    memset(buffer, '\0', sizeof(buffer));
-    while ((received = recv(socket, buffer, size, flags)) > 0) {
-        if (received == -1)
-            SysErr("recv");
-        fprintf(stdout, "%s", buffer);
-        totalReceived += received;
-        bzero(buffer, received);
+int send_buffer(int fd, const char *buffer, size_t n, int flags) {
+    size_t len = n;
+    ssize_t total = 0, sent = 0;
+    while (len > 0) {
+        if ((sent = send(fd, buffer + total, len, flags)) == -1)
+            sys_err("send");
+        total += sent;
+        len -= (size_t)sent;
     }
-    fflush(stdout); // flush stdout buffer
-    return totalReceived;
+    return (int)total;
+}
+int send_file(int out_fd, int in_fd, size_t count) {
+    off_t offset = 0;
+    size_t len = count;
+    ssize_t total = 0, sent = 0;
+    while (len > 0) {
+        if ((sent = sendfile(out_fd, in_fd, &offset, len)) == -1)
+            sys_err("send_file");
+        total += sent;
+        len -= (size_t)sent;
+    }
+    return (int)total;
+}
+int recv_buffer(int fd, char *buffer, size_t n, int flags) {
+    ssize_t total = 0, nrecv = 0;
+    while ((nrecv = recv(fd, buffer, n, flags)) != 0) {
+        if (nrecv == -1)
+            sys_err("recv");
+        total += nrecv;
+        fprintf(stdout, "%.*s", (int)nrecv, buffer);
+        if (strstr(buffer, "\r\n\r\n") != NULL)
+            break;
+    }
+    fflush(stdout);
+    return (int)total;
 }
