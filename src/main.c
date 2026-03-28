2ebff23d8ec660f82550e79ba43241b1a590a742
DamarSurya <hexazed1@gmail.com>
2026-03-28 18:46:05 +0700

    add ipv6.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define DEFAULT_ADDRESS ((in_addr_t)INADDR_ANY)
+#define DEFAULT_ADDRESS ((const char *)"0.0.0.0")
 #define DEFAULT_PORT ((uint16_t)3000)
 #define MAX_BUFFER_SIZE ((size_t)4096)
 #define USAGE_MESSAGE                                                          \
@@ -23,12 +23,12 @@
     \t\t-l: \tSet to listen mode\n\
     \t\t-p: \tSet port\n\
     \t\t-i: \tSet ip\n\
+    \t\t-6: \tEnable ipv6\n\
     \t\t-r: \tLooped listen mode\n\
     \t\t-m: \tSet message\n\
     \t\t-b: \tSet max_backlog or maximum connection\n\
     \tRedirection: ./[program] [-l > out] [ [ip] [port] < in]\n"
 #define INACTIVE_SOCKET ((int)-42)
-
 bool listen_mode = false;
 bool verbose_mode = false;
 
@@ -65,24 +65,22 @@ int main(int argc, char **argv) {
         fprintf(stderr, "[ERROR]: Not enough arguments\n%s", USAGE_MESSAGE);
         exit(-1);
     }
-    bool ip_is_set = false;
-    bool port_is_set = false;
+    bool is_ipv6 = false;
     bool message_is_set = false;
     bool listen_is_looped = false;
-
-    in_addr_t local_address = DEFAULT_ADDRESS;
-    uint16_t local_port = htons(DEFAULT_PORT);
+    const char *address = DEFAULT_ADDRESS;
+    uint16_t port = htons(DEFAULT_PORT);
     const char *message_ptr = "\0";
 
     int domain = AF_INET;
     int socket_type = SOCK_STREAM;
-    int protocol = 0;
+    int protocol = IPPROTO_TCP;
     int max_backlog = 10;
     int enableREUSEADDR = 1;
 
     opterr = 0;
     int opt, nargs = 0;
-    while ((opt = getopt(argc, argv, "lhvri:p:m:b:")) != -1) {
+    while ((opt = getopt(argc, argv, "lhvri:p:m:b:6")) != -1) {
         ++nargs;
         switch (opt) {
         case 'h':
@@ -103,12 +101,14 @@ int main(int argc, char **argv) {
             listen_is_looped = true;
             break;
         case 'i':
-            ip_is_set = true;
-            local_address = inet_addr(optarg);
+            address = optarg;
             break;
         case 'p':
-            port_is_set = true;
-            local_port = htons((uint16_t)atoi(optarg));
+            port = htons((uint16_t)atoi(optarg));
+            break;
+        case '6':
+            is_ipv6 = true;
+            domain = AF_INET6;
             break;
         case 'b':
             max_backlog = atoi(optarg);
@@ -121,10 +121,24 @@ int main(int argc, char **argv) {
     if (listen_mode) {
         log_verb("[LOG]: On listen mode\n");
         int local_socket = INACTIVE_SOCKET;
-        struct sockaddr_in local_socket_address;
-        local_socket_address.sin_addr.s_addr = local_address;
-        local_socket_address.sin_port = local_port;
-        local_socket_address.sin_family = domain;
+        struct sockaddr_storage local_socket_address;
+        socklen_t socket_size;
+        if (is_ipv6) {
+            struct sockaddr_in6 *addr =
+                (struct sockaddr_in6 *)&local_socket_address;
+            if (inet_pton(AF_INET6, address, &addr->sin6_addr) == -1)
+                sys_err("inet_pton");
+            addr->sin6_family = domain;
+            addr->sin6_port = port;
+            socket_size = sizeof(struct sockaddr_in6);
+        } else {
+            struct sockaddr_in *addr =
+                (struct sockaddr_in *)&local_socket_address;
+            addr->sin_addr.s_addr = inet_addr(address);
+            addr->sin_port = port;
+            addr->sin_family = domain;
+            socket_size = sizeof(struct sockaddr_in);
+        }
         if ((local_socket = socket(domain, socket_type, protocol)) == -1)
             sys_err("socket");
         if (setsockopt(local_socket, SOL_SOCKET, SO_REUSEADDR,
@@ -132,15 +146,16 @@ int main(int argc, char **argv) {
                        (socklen_t)sizeof(enableREUSEADDR)) == -1)
             sys_err("setsockopt");
         if (bind(local_socket, (struct sockaddr *)&local_socket_address,
-                 sizeof(local_socket_address)) == -1)
+                 socket_size) == -1)
             sys_err("bind");
         if (listen(local_socket, max_backlog) == -1)
             sys_err("listen");
-        flog_verb("[LOG]: Running on: \n\tip: %s\n\tport: %d\n\tmessage "
-                  "length: %lu\n",
-                  inet_ntoa(local_socket_address.sin_addr), ntohs(local_port),
-                  strlen(message_ptr));
-
+        flog_verb("[LOG]: Running on: \n\tIP Mode: %s\n\tIPPROTO: %s\n\tIp: "
+                  "%s\n\tPort: %d\n\tMax_backlog: "
+                  "%d\n\tmessage "
+                  "length: %lu\n\n",
+                  ((is_ipv6) ? "IPV6" : "IPV4"), ("TCP/IP"), address,
+                  ntohs(port), max_backlog, strlen(message_ptr));
         while (1) {
             int peer_socket = INACTIVE_SOCKET;
             struct sockaddr peer_socket_addr;
@@ -154,13 +169,13 @@ int main(int argc, char **argv) {
             peer_addr = (struct sockaddr_in *)&peer_socket_addr;
             char buffer[MAX_BUFFER_SIZE];
             int len = recv_buffer(peer_socket, buffer, MAX_BUFFER_SIZE, 0);
-            log_verb("[LOG]: Accepting connection");
+            log_verb("Accepting connection");
             flog_verb("[LOG]: Connection info: \n\tip: %s\n\tport: "
                       "%u\n\tmessage length received: %d\n\n",
                       inet_ntoa(peer_addr->sin_addr),
                       ntohs(peer_addr->sin_port), len);
-
             shutdown(peer_socket, SHUT_RD);
+
             if (message_is_set) {
                 log_verb("sending message...");
                 send_buffer(peer_socket, message_ptr, strlen(message_ptr), 0);
@@ -173,35 +188,49 @@ int main(int argc, char **argv) {
         close(local_socket);
         return 0;
     }
-
     // sending mode
-    log_verb("On sending mode");
+    log_verb("On Sending Mode");
     if (argc < 3) {
         fprintf(stderr, "[ERROR]: Not enough arguments\n%s", USAGE_MESSAGE);
         return -1;
     }
+    address = argv[argc - 2];
+    port = htons(atoi(argv[argc - 1]));
     int local_socket = INACTIVE_SOCKET;
-    struct sockaddr_in target_socket_addr;
-    target_socket_addr.sin_addr.s_addr = inet_addr(argv[nargs + 1]);
-    target_socket_addr.sin_port = htons(atoi(argv[nargs + 2]));
-    target_socket_addr.sin_family = domain;
+    struct sockaddr_storage target_socket_addr;
+    socklen_t socket_size;
+    if (is_ipv6) {
+        struct sockaddr_in6 *addr = (struct sockaddr_in6 *)&target_socket_addr;
+        if (inet_pton(domain, address, &addr->sin6_addr) == -1)
+            sys_err("inet_pton");
+        addr->sin6_family = domain;
+        addr->sin6_port = port;
+        socket_size = sizeof(struct sockaddr_in6);
+    } else {
+        struct sockaddr_in *addr = (struct sockaddr_in *)&target_socket_addr;
+        addr->sin_addr.s_addr = inet_addr(address);
+        addr->sin_port = port;
+        addr->sin_family = domain;
+        socket_size = sizeof(struct sockaddr_in);
+    }
     if ((local_socket = socket(domain, socket_type, protocol)) == -1)
         sys_err("socket");
-    flog_verb("[LOG]: Connecting to: %s:%s\n",
-              inet_ntoa(target_socket_addr.sin_addr), argv[3]);
+    flog_verb("\tIP Mode: %s\n\tIPPROTO: %s\n\tConnecting to: %s:%s\n\n",
+              ((is_ipv6) ? "IPV6" : "IPV4"), ("TCP/IP"), address,
+              argv[argc - 1]);
     if (connect(local_socket, (struct sockaddr *)&target_socket_addr,
-                sizeof(target_socket_addr)) == -1)
+                socket_size) == -1)
         sys_err("connect");
     log_verb("Connection established.");
 
     if (message_is_set) {
-        flog_verb("Sending message. len: %lu\n", strlen(message_ptr));
+        flog_verb("[LOG]: Sending message. length: %lu\n", strlen(message_ptr));
         send_buffer(local_socket, message_ptr, strlen(message_ptr), 0);
     }
     if (isatty(STDIN_FILENO) == 0) {
         struct stat buf_info;
         fstat(STDIN_FILENO, &buf_info);
-        flog_verb("Sending file. File size: %lu\n", buf_info.st_size);
+        flog_verb("[LOG]: Sending file. File size: %lu\n", buf_info.st_size);
         send_file(local_socket, 0, buf_info.st_size);
     }
     shutdown(local_socket, SHUT_WR);
